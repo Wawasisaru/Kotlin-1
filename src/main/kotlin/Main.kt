61aeb952be6bab9c678d9f2ebb65883af0071fab
Wawasisaru <nsaruwawasi@gmil,.com>
2023-02-02 17:45:56 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+
+    val name = "Mercy John"
+    val age = "21"
+    val phoneNumber = "+245706007173"
+    val weightInKg = "60"
+    val citizenshipKenyan = true
+
+    println(name)
+    println(age)
+    println(weightInKg)
+    println(phoneNumber)
+    println(citizenshipKenyan)
+
+
+}
